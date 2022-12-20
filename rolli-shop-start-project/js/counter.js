241206ff51d1c37278bdd3761a1ccad594a11dd7
kurkuroomatkerimov <kurkuroomatkerimov@gmail.com>
2022-12-20 21:35:49 +0600

    create base counter

--- rolli-shop-start-project/js/counter.js
+++ rolli-shop-start-project/js/counter.js
@@ -8,7 +8,10 @@ const counter = document.querySelector('[data-counter]')
 buttonMinus.addEventListener('click', function () {
 console.log('minus')
 
+
+    // Проверяем чтобы счетчик был больше 1
     if (counter.innerText > 1){
+        // Изменяем текст в счетчике уменьшая его на 1
         counter.innerText = --counter.innerText
     }
 
