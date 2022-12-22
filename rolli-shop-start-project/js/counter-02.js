d108aa0779375db20dcbbbbb43c6cda114222050
kurkuroomatkerimov <kurkuroomatkerimov@gmail.com>
2022-12-22 18:17:50 +0600

    removing an item from the shopping cart

--- rolli-shop-start-project/js/counter-02.js
+++ rolli-shop-start-project/js/counter-02.js
@@ -6,29 +6,42 @@ window.addEventListener('click', function (event) {
 
 
     // Проверяем клик строго по кнопкам Плюс или Минус
-    if (event.target.dataset.action === 'plus' || event.target.dataset.action === 'minus'){
+    if (event.target.dataset.action === 'plus' || event.target.dataset.action === 'minus') {
 
         // Находим обертку сччетчика (родительский элемент)
         const counterWrapper = event.target.closest('.counter-wrapper')
         // Находим див с числом счетчика (дочерний элемент)
-         counter = counterWrapper.querySelector('[data-counter]')
+        counter = counterWrapper.querySelector('[data-counter]')
     }
 
-
     // Проверяем является ли елемент кнопкой Плюс
-    if (event.target.dataset.action === 'plus'){
+    if (event.target.dataset.action === 'plus') {
         counter.innerText = ++counter.innerText
     }
 
     // Проверяем является ли елемент кнопкой Минус
-    if (event.target.dataset.action === 'minus'){
+    if (event.target.dataset.action === 'minus') {
+
+        // // Проверка на товар  который находится в корзине
+        // if (event.target.closest('.cart-wrapper') && parseInt(counter.innerText) === 1) {
+        //     console.log("in cart")
+        //     // Удаляем товар из корзины
+        //     event.target.closest('.cart-item').remove()
+        // }
 
         // Проверяем чтобы счетчик был больше 1
-        if (counter.innerText > 1){
+        if (parseInt(counter.innerText) > 1) {
             // Изменяем текст в счетчике уменьшая его на 1
             counter.innerText = --counter.innerText
+        } else if (event.target.closest('.cart-wrapper') && parseInt(counter.innerText) === 1{
+            // Проверка на товар  который находится в корзине
+            console.log("in cart")
+            // Удаляем товар из корзины
+            event.target.closest('.cart-item').remove()
+
+
         }
 
-    }
 
+    }
 })
